bc1d5070b0e70d72685fe8c39e04ba4ca92b89fb
LeoZeng <2570613257@qq.com>
2019-12-13 10:43:27 +0800

    新建develop支线

--- framework/logger-service/src/main/java/com/godarmed/core/framework/logger/LoggerApplication.java
+++ framework/logger-service/src/main/java/com/godarmed/core/framework/logger/LoggerApplication.java
@@ -1,6 +1,5 @@
 package com.godarmed.core.framework.logger;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
