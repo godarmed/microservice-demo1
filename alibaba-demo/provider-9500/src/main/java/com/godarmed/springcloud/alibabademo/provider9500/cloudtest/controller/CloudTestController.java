c5bcd2f272bd44660e34721a1f25da3d452b8f76
LeoZeng <2570613257@qq.com>
2020-04-20 18:31:03 +0800

    feature: nacos-注册中心-雏形-sp1

--- alibaba-demo/provider-9500/src/main/java/com/godarmed/springcloud/alibabademo/provider9500/cloudtest/controller/CloudTestController.java
+++ alibaba-demo/provider-9500/src/main/java/com/godarmed/springcloud/alibabademo/provider9500/cloudtest/controller/CloudTestController.java
@@ -1,6 +1,7 @@
 package com.godarmed.springcloud.alibabademo.provider9500.cloudtest.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @Created by Administrator
  */
 @RestController
+@RefreshScope   //支持Nacos的动态刷新
 public class CloudTestController {
 
     @Value("${server.port}")
     private String serverPort;
 
+    @Value("${config.info}")
+    private String configInfo;
+
     @GetMapping(value = "/cloudTest/{id}")
     public String getServerInfo(@PathVariable("id") Integer id){
         return "nacos registry, serverPort: " + serverPort + "\t id " + id;
+    }
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
     }
 }
